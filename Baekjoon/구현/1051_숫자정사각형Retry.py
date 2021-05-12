8587a68f294069fd1cc6a32f2865f33777911f64
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-05-12 19:33:52 +0900

    210512

--- Baekjoon/구현/1051_숫자정사각형Retry.py
+++ Baekjoon/구현/1051_숫자정사각형Retry.py
@@ -1,34 +1,61 @@
+# 210512 재도전 했는데 2시간 이상 걸려서 풀고 + 다른 반례나 답도 본 관계로 이해 못한걸로 판정
+
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
 
-board = []
-for _ in range(n) :
-    arr = list(map(int, sys.stdin.readline().rstrip())) # n*m 직사각형 생성
-    board.append(arr)
-
-
-# 해당 직사각형내에서 유효한 정사각형 범위 limit를 지정한다
-if n < m :
-    limit = n
-else :
-    limit = m
-
-# 적정 정사각형을 찾았을때의, 그 사각형의 변의 길이
-maximum = 0
-# limit = min(n, m)
-
+graph = []
 for i in range(n) :
-    for j in range(m) :
-        for k in range(limit) :
-            if i + k < n and j + k < m :
-                # 만약 limit길이를 가진 정사각형을 찾게되면, maximum을 새로 갱신한다
-                if board[i][j] == board[i][j+k] and board[i][j] == board[i+k][j] and board[i][j] == board[i+k][j+k] :
-                    if maximum < k:
-                        maximum = k
+    tmp = sys.stdin.readline().rstrip()
+    graph.append(tmp)
 
-# 정사각형넓이 구함
-print((maximum+1) ** 2)
+if n < m :
+    stand = n
+
+else :
+    stand = m
+
+maximum = 1
+
+for k in range(stand) :
+    for i in range(n) :
+        for j in range(m) :        
+            if i+k < n and j+k < m :
+                if graph[i][j] == graph[i+k][j] == graph[i][j+k] == graph[i+k][j+k] :
+                    maximum = k+1
+
+print(maximum**2)
+
+
+# n, m = map(int, sys.stdin.readline().split())
+
+# board = []
+# for _ in range(n) :
+#     arr = list(map(int, sys.stdin.readline().rstrip())) # n*m 직사각형 생성
+#     board.append(arr)
+
+
+# # 해당 직사각형내에서 유효한 정사각형 범위 limit를 지정한다
+# if n < m :
+#     limit = n
+# else :
+#     limit = m
+
+# # 적정 정사각형을 찾았을때의, 그 사각형의 변의 길이
+# maximum = 0
+# # limit = min(n, m)
+
+# for i in range(n) :
+#     for j in range(m) :
+#         for k in range(limit) :
+#             if i + k < n and j + k < m :
+#                 # 만약 limit길이를 가진 정사각형을 찾게되면, maximum을 새로 갱신한다
+#                 if board[i][j] == board[i][j+k] and board[i][j] == board[i+k][j] and board[i][j] == board[i+k][j+k] :
+#                     if maximum < k:
+#                         maximum = k
+
+# # 정사각형넓이 구함
+# print((maximum+1) ** 2)
 
 
 # --------------------------------------------------------------------------------------------------------------------
