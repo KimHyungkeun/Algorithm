a0aa7db6e6f9284456eb12db91e30be5c9b5e384
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-05-17 23:26:23 +0900

    210517

--- Baekjoon/DFS,BFS/7576_토마토.py
+++ Baekjoon/DFS,BFS/7576_토마토.py
@@ -1,81 +1,152 @@
+# 210517 재풀이
 import sys
 from collections import deque
 
-# 가로 n, 세로 m의 토마토 틀
-n, m = map(int, sys.stdin.readline().split())
+m, n = map(int, sys.stdin.readline().split())
+
+minus_one_cnt = 0
+zero_cnt = 0
+one_cnt = 0
 board = []
-queue = deque()
 
-# 현위치에서의 좌,우,하,상 
-dx = [-1,1,0,0] 
-dy = [0,0,-1,1]
-day = 0 # 지난 횟수
+visited = []
+day = 0
 
-cnt_one = 0 # 1의 갯수(익은 토마토)
-cnt_m_one = 0 # -1의 갯수(빈칸)
+for _ in range(n) :
+    tomato = list(map(int, sys.stdin.readline().split()))
+    board.append(tomato)
 
-
-# 토마토 넣음
-for _ in range(m) :
-    row = list(map(int, sys.stdin.readline().split()))
-    board.append(row)
-
-# 1의 갯수와 -1의 갯수를 센다
-for i in range(m) :
-    for j in range(n) :
+start = []
+for i in range(len(board)) :
+    for j in range(len(board[i])) :
         if board[i][j] == 1 :
-            cnt_one += 1
-            queue.append((i,j))
-        elif board[i][j] == -1 :
-            cnt_m_one += 1
+            start.append((i,j))
 
-# 만약, 처음부터 빈칸을 제외하고 익은토마토만이 존재한다면 0을 출력
-if cnt_one + cnt_m_one == m * n :
-    print(0)
+q = deque(start)
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+length = len(q)
+while q :
+    x, y = q.popleft()
+    length -= 1
+
+    for d in range(4) :
+        px = x + dx[d]
+        py = y + dy[d]
+        if 0 <= px < n and 0 <= py < m :
+            if board[px][py] == 0 :
+                board[px][py] = 1
+                q.append((px, py))
+            
+
+    if length == 0 :
+        day += 1
+
+        if not q :
+            break
+
+        length = len(q)
+        # print(length, q, day)
+        
+            
+    # for b in board :
+    #     print(b)
+    # print("--------------")
+
+
+for tomato in board :
+    minus_one_cnt += tomato.count(-1)
+    zero_cnt += tomato.count(0)
+    one_cnt += tomato.count(1)
+
+if zero_cnt >= 1 :
+    print(-1)
+
+elif minus_one_cnt == m*n :
+    print(-1)
 
 else :
-    while queue :
+    print(day-1)
 
-        j = 0
-        length = len(queue)
-        # print(queue)
 
-        # 해당날의 익은 토마토를 탐색하는 프로그램
-        while j != length :
-            x, y = queue.popleft()
-            for i in range(4) :
-                a = x + dx[i]
-                b = y + dy[i]
 
-                if 0 <= a < m and 0 <= b < n :
-                    # 만약 이미 익은 토마토라면 다른 위치를 탐색한다
-                    if board[a][b] == -1 or board[a][b] == 1:
-                        continue
-                    # 만약 이번에 새로 익을 토마토라면, 위치를 저장하고 다음 비교를 위해 새 위치도 저장한다
-                    if board[a][b] == 0 :
-                        board[a][b] = 1
-                        queue.append((a,b))
-            j += 1
+# # 가로 n, 세로 m의 토마토 틀
+# n, m = map(int, sys.stdin.readline().split())
+# board = []
+# queue = deque()
+
+# # 현위치에서의 좌,우,하,상 
+# dx = [-1,1,0,0] 
+# dy = [0,0,-1,1]
+# day = 0 # 지난 횟수
+
+# cnt_one = 0 # 1의 갯수(익은 토마토)
+# cnt_m_one = 0 # -1의 갯수(빈칸)
+
+
+# # 토마토 넣음
+# for _ in range(m) :
+#     row = list(map(int, sys.stdin.readline().split()))
+#     board.append(row)
+
+# # 1의 갯수와 -1의 갯수를 센다
+# for i in range(m) :
+#     for j in range(n) :
+#         if board[i][j] == 1 :
+#             cnt_one += 1
+#             queue.append((i,j))
+#         elif board[i][j] == -1 :
+#             cnt_m_one += 1
+
+# # 만약, 처음부터 빈칸을 제외하고 익은토마토만이 존재한다면 0을 출력
+# if cnt_one + cnt_m_one == m * n :
+#     print(0)
+
+# else :
+#     while queue :
+
+#         j = 0
+#         length = len(queue)
+#         # print(queue)
+
+#         # 해당날의 익은 토마토를 탐색하는 프로그램
+#         while j != length :
+#             x, y = queue.popleft()
+#             for i in range(4) :
+#                 a = x + dx[i]
+#                 b = y + dy[i]
+
+#                 if 0 <= a < m and 0 <= b < n :
+#                     # 만약 이미 익은 토마토라면 다른 위치를 탐색한다
+#                     if board[a][b] == -1 or board[a][b] == 1:
+#                         continue
+#                     # 만약 이번에 새로 익을 토마토라면, 위치를 저장하고 다음 비교를 위해 새 위치도 저장한다
+#                     if board[a][b] == 0 :
+#                         board[a][b] = 1
+#                         queue.append((a,b))
+#             j += 1
         
-        # 만약, 당일날의 토마토 익음 유무를 모두 탐색했다면 그 다음날로 넘어간다
-        if length == j :
-            j = 0
-            day += 1
+#         # 만약, 당일날의 토마토 익음 유무를 모두 탐색했다면 그 다음날로 넘어간다
+#         if length == j :
+#             j = 0
+#             day += 1
     
-    # 익지 않은 토마토 진위 여부
-    contain_zero = False
+#     # 익지 않은 토마토 진위 여부
+#     contain_zero = False
 
-    for i in range(m) :
-        if 0 in board[i] :
-            contain_zero = True
-            break
-    # 만약, 여전히 익지 않은 토마토가 존재한다면 -1을 출력한다
-    if contain_zero or cnt_m_one == m * n :
-        print(-1)
+#     for i in range(m) :
+#         if 0 in board[i] :
+#             contain_zero = True
+#             break
+#     # 만약, 여전히 익지 않은 토마토가 존재한다면 -1을 출력한다
+#     if contain_zero or cnt_m_one == m * n :
+#         print(-1)
     
-    # 모두 다 익었다면 지난 날을 출력
-    else :
-        print(day-1)
+#     # 모두 다 익었다면 지난 날을 출력
+#     else :
+#         print(day-1)
             
         
 # --------------------------------------------------------------------------------------------------------------------
