6797a3cd0fddd38eac914c60d10bee7f9f2bfa0b
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-06-24 20:15:29 +0900

    210624

---
+++ HackerRank/Practice/Java/Introduction/Java_Stdin_and_Stdout2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        int integer = sc.nextInt(); 
+        double doubles = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();       
+        
+       
+        
+        
+        System.out.println("String: " + str);
+        System.out.println("Double: " + doubles);
+        System.out.println("Int: " + integer);
+        
+    }
+}
