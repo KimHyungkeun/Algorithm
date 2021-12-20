8525ef7c3188b64665a5f1f6f5e32795c7cee1a4
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-12-20 13:59:50 +0900

    211220

---
+++ PythonAlgorithmInterview/트리/104_Maximum_depth_of_Binary_Tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None :
+            return 0
+        
+     
+        q = deque([root])
+        depth = 0
+        
+        while q :
+            depth += 1
+            # 큐 연산 추출 노드의 자식 노드 삽입
+            for _ in range(len(q)) :
+                cur_root = q.popleft()
+                if cur_root.left:
+                    q.append(cur_root.left)
+                if cur_root.right:
+                    q.append(cur_root.right)
+        
+        # BFS 반복 횟수 == 깊이
+        return depth
