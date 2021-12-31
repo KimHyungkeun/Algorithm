b46bd24e6278d64871195955934c60f4cea1cb02
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-12-31 22:09:47 +0900

    211231

--- Softeer/level2/비밀메뉴(Retry).py
+++ Softeer/level2/비밀메뉴(Retry).py
@@ -6,7 +6,7 @@ total_num = list(map(int, sys.stdin.readline().split()))
 
 flag = False
 if len(sec_num) <= len(total_num) and max(total_num) <= k :
-    for i in range(len(total_num) - m) :
+    for i in range(len(total_num) - m) : # 원인 : for문 범위를 +1 더해야함. 예를 들어 비밀번호 길이가 3, 전체번호 길이가 10일때 탐색 범위의 시작을 0 ~ 7로 맞추어야 하므로 10 - 3 + 1 = 8이 되어야 한다
         if total_num[i:i+m] == sec_num[:] :
             flag = True
             break
@@ -24,7 +24,7 @@ sec_num = list(map(int, sys.stdin.readline().split()))
 total_num = list(map(int, sys.stdin.readline().split()))
 
 flag = False
-if len(sec_num) <= len(total_num) and max(total_num) <= k :
+if len(sec_num) <= len(total_num) :
     for i in range(n) :
         if sec_num[:] == total_num[i:i+m] :
             flag = True
