4579c1c4b5b58cfcdf47e606621ec3c8088c2a9e
KimHyungkeun <hyungkeun.kim95@gmail.com>
2022-01-07 22:35:47 +0900

    220107

--- PythonAlgorithmInterview/트리/104_Maximum_depth_of_Binary_Tree.py
+++ PythonAlgorithmInterview/트리/104_Maximum_depth_of_Binary_Tree.py
@@ -19,3 +19,44 @@ class Solution:
         
         # BFS 반복 횟수 == 깊이
         return depth
+
+# DFS 풀이
+class Solution:
+    max_depth = 0
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+        if root is None :
+            return 0
+        
+        depth = 0
+        def dfs(node, depth) :
+            
+            if node is None :
+                self.max_depth = max(self.max_depth, depth)
+                return
+                        
+            dfs(node.left, depth+1)      
+            dfs(node.right, depth+1)
+                
+
+        dfs(root, depth)  
+        return self.max_depth
+
+# DFS 풀이2
+class Solution:
+    max_depth = 0
+    
+    def dfs(self, node, depth) :                 
+        if node is None :
+            self.max_depth = max(self.max_depth, depth)
+            return
+        self.dfs(node.left, depth+1)                  
+        self.dfs(node.right, depth+1)
+            
+    def maxDepth(self, root: Optional[TreeNode]) -> int:        
+        if root is None :
+            return 0
+        
+        depth = 0
+        self.dfs(root, depth)  
+        return self.max_depth
