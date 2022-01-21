3655ff060d8519e3134a881203eb4d604fe1bb3e
KimHyungkeun <hyungkeun.kim95@gmail.com>
2022-01-21 14:54:03 +0900

    220121

--- PythonAlgorithmInterview/분할정복/169_Majority_Elements.py
+++ PythonAlgorithmInterview/분할정복/169_Majority_Elements.py
@@ -12,3 +12,21 @@ class Solution:
         # 가장 많이 등장하는 숫자를 리턴
         max_key = max(num_dict.items(), key = lambda x : x[1])
         return max_key[0]
+
+
+# 분할정복
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        if not nums :
+            return 
+        if len(nums) == 1 :
+            return nums[0]
+        
+        mid = len(nums) // 2
+        a = self.majorityElement(nums[:mid])
+        b = self.majorityElement(nums[mid:])
+        
+        if nums.count(a) > mid :
+            return a
+        else :
+            return b
