31df5a4eabb30d3a21007e3d313441a7aa9d9735
KimHyungkeun <hyungkeun.kim95@gmail.com>
2022-02-06 10:32:59 +0900

    220206

--- Programmers/Python/2022/연습문제/이중우선순위큐.py
+++ Programmers/Python/2022/연습문제/이중우선순위큐.py
@@ -30,3 +30,34 @@ def solution(operations):
             
             
     return answer
+
+# 220206 풀이
+import heapq
+def solution(operations):
+    heap = []
+    max_heap = []
+    
+    for o in operations :
+        cmd = o.split()
+        num = int(cmd[1])
+        if cmd[0] == "I" :
+            heapq.heappush(heap, num)
+            heapq.heappush(max_heap, (-num, num))
+        
+        else :
+            if not heap :
+                continue
+            
+            if num == -1 :
+                min_val = heapq.heappop(heap)
+                max_heap.remove((-min_val,min_val))
+                
+            else :
+                max_val = heapq.heappop(max_heap)[1]
+                heap.remove(max_val)
+                
+        
+    if not heap :
+        return [0,0]
+    else :
+        return [heapq.heappop(max_heap)[1], heapq.heappop(heap)]
