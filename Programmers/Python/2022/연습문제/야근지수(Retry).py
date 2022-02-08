0a678715f012bf0665c65e269a2ce29eca2ab12c
KimHyungkeun <hyungkeun.kim95@gmail.com>
2022-02-08 19:22:51 +0900

    220208

---
+++ Programmers/Python/2022/연습문제/야근지수(Retry).py
@@ -0,0 +1,25 @@
+import heapq
+def solution(n, works):
+    # 일해야하는 시간이 n보다 작으면 야근 안해도 된다
+    if sum(works) < n :
+        return 0
+    
+    # 근무를 먼저 끝내야하는 우선순위를 정한다
+    heap = []
+    for w in works :
+        heapq.heappush(heap, [-w, w])
+    
+    # n시간이 지날때 까지 근무를 계속한다
+    while n != 0 :
+        work = heapq.heappop(heap)
+        if work != (0,0) :
+            work[0] += 1
+            work[1] -= 1
+        heapq.heappush(heap, work)
+        n -= 1
+    
+    # 이 때, 야근 지수는 현재 heap에 담긴 h의 제곱들의 합이다
+    ans = 0
+    for h in heap :
+        ans += h[1]**2
+    return ans
