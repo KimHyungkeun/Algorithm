0aea88764071512baaba490dcdbdbd2de319144a
KimHyungkeun <hyungkeun.kim95@gmail.com>
2022-02-18 17:40:09 +0900

    220218

---
+++ HackerRank/ETC/Floor_Ceil_Rint.py
@@ -0,0 +1,10 @@
+import numpy
+import sys
+
+numpy.set_printoptions(sign=' ')
+my_arr = numpy.array(input().split(),float)
+
+
+print(numpy.floor(my_arr))
+print(numpy.ceil(my_arr))
+print(numpy.rint(my_arr))
